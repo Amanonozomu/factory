9ee1f743c94a5605b121c438e010e4daf3cd9ad3
Amanonozomu <0nozomu0@gmail.com>
2021-03-25 17:33:52 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
 window.addEventListener('pageshow', function(event) {
     document.getElementById("impressiom_mask").style.color='#0000ff';
+    console.log(document.getElementById("impressiom_mask").style);
 });
