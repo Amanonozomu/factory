e7135ac8d0051d8286eb64c654f212274a26aa34
Amanonozomu <0nozomu0@gmail.com>
2021-03-25 17:57:16 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+document.addEventListener('click', function(){
+    document.getElementById("impressiom_mask").style.color='#00ff00';
+});
 window.addEventListener('pageshow', function(event) {
     const maskID = document.getElementById("impressiom_mask");
     if(maskID) {
