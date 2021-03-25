925758e9c23e2ef08b93b09d8d923b86e85c32fc
Amanonozomu <0nozomu0@gmail.com>
2021-03-25 17:42:05 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-
-const maskID = document.getElementById("impressiom_mask");
 window.addEventListener('pageshow', function(event) {
+    const maskID = document.getElementById("impressiom_mask");
     if(maskID) {
         document.getElementById("impressiom_mask").style.color='#0000ff';
         console.log(document.getElementById("impressiom_mask").style.color);
