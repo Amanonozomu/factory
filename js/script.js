69b4c75a93b2b12d038fac18596176b7f220bddc
Amanonozomu <0nozomu0@gmail.com>
2021-03-25 17:30:48 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 
-const submitButton = document.querySelector('#impressiom_mask');
-submitButton.addEventListener('click', () => {
-    document.getElementById("impressiom_mask").style.color='#00ff00';
-});
 window.addEventListener('pageshow', function(event) {
-    document.getElementById("impressiom_mask").style.color='#ff0000';
+    document.getElementById("impressiom_mask").style.color='#0000ff';
 });
