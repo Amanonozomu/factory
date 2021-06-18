ed500255e95fc9ad5d3782fa16ff67ba7e87e662
Amanonozomu <0nozomu0@gmail.com>
2021-06-18 10:46:02 +0900

    Noczomu
    test

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.querySelector(iframe).addEventListener('contextmenu', contextmenu);
+document.querySelector('.video').addEventListener('contextmenu', contextmenu);
 
 function contextmenu(e) {
 	e.preventDefault();
